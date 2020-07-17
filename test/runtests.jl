915a7e8a1633416ebbd57e2ed95cc6ce7e0d4e24
lrennels <lrennels@berkeley.edu>
2020-07-16 19:09:37 -0700

    Add delta method testing skeleton

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,7 @@
 using Test
 
-@testset "Test Delta Method" begin
-    # TODO
-end 
-
 @testset "Test Sobol Method" begin
+
     @testset "Compare to SALib.py" begin
         include("sobol/test_sobolsequence.jl")
         include("sobol/test_uniform.jl")
@@ -14,4 +11,17 @@ end
     @testset "Unit Testing" begin
         include("sobol/unit_tests.jl")
     end
+
+@testset "Test Delta Method" begin
+
+    @testset "Compare to SALib.py" begin
+        # TODO
+    end
+
+    @testset "Unit Testing" begin
+        # TODO
+    end
+
+end
+
 end
